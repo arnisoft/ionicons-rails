8db11ec69d44f586eafc6cb8cf06c4206e0bdd26
arni <arnisoft@yandex.ru>
2021-01-18 00:01:11 +0300

    Гем откачен до 2.0.0 и пропатчен под sprockets v4

--- lib/ionicons-rails.rb
+++ lib/ionicons-rails.rb
@@ -2,7 +2,7 @@ module Ionicons
   module Rails
     class Engine < ::Rails::Engine
       initializer "ionicons-rails.assets.precompile" do |app|
-        app.config.assets.precompile << %r(ionicons\.(?:eot|svg|ttf|woff)$)
+        app.config.assets.precompile += %w(ionicons.eot ionicons.svg ionicons.ttf ionicons.woff)
       end
     end
   end
